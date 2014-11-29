6464a901e115a5e04d2b6732161f4b5dab250257
evancchow <echow@princeton.edu>
2014-11-28 21:13:43 -0500

    Initial commit.

---
+++ fetch.js
@@ -0,0 +1,12 @@
+/* Fetch a url to a machine learning paper from Google Scholar. */
+
+function fetch() {
+    
+}
+
+/* Create a content loaded listener, so that the extension runs as
+    soon as you load the page. */
+
+document.addEventListener('DOMContentLoaded', function() {
+    
+});
